4462a6bc9de80671d66c9de36b77559b50632803
bakytjanmuktarov <bakytjan.q@gmail.com>
2026-09-24 11:40:13 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
         int n = in.nextInt();
-        int hours = (n/3600)%60;
+        int hours = (n/3600)%24;
         int minutes = (n/60)%60;
         int seconds = n%60;
-        System.out.println(hours + ":"+ minutes+":"+seconds);
+        System.out.printf("%d:%02d:%02d\n" ,hours, minutes, seconds);
     }
 }
